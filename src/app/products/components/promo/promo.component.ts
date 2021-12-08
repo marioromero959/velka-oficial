f2a593459f340aaced66f9af40a57df873c1b9e2
marioromero959 <marioromero959@gmail.com>
2021-12-08 11:01:29 -0300

    Maquetado promociones

--- src/app/products/components/promo/promo.component.ts
+++ src/app/products/components/promo/promo.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { product } from 'src/app/interface';
+import { ProductsService } from 'src/app/services/products.service';
+import Swiper, { Navigation, Pagination,Autoplay } from 'swiper';
 
 @Component({
   selector: 'app-promo',
   templateUrl: './promo.component.html',
   styleUrls: ['./promo.component.scss']
 })
-export class PromoComponent implements OnInit {
+export class PromoComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
+  products:product[];
+
+  constructor(private productSvc:ProductsService) { }
 
   ngOnInit(): void {
+    this.products = this.productSvc.getAllProducts() 
+  }
+
+  ngAfterViewInit(){
+    Swiper.use([Navigation, Pagination,Autoplay]);
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      loop: true,
+      autoplay:{
+        disableOnInteraction:false,
+        delay:3000,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
 
 }
