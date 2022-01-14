cd0104ee9754e331e5dc24a4b8023e5cffa3f006
Mario Romero <marioromero959@gmail.com>
2022-01-14 14:45:57 -0300

    agregando vistas anidadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-<<<<<<< HEAD
 import { FooterComponent } from './footer/components/footer.component';
-=======
->>>>>>> 21e2a19e99d7d5d612f668d38dbe0d6f03b8689a
 import { MaterialModule } from './material/material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/components/login/login.component';
@@ -16,14 +13,11 @@ import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
     AppComponent,
     FooterComponent,
     LoginComponent,
-    LayoutComponent
-=======
+    LayoutComponent,
     AppComponent
->>>>>>> 21e2a19e99d7d5d612f668d38dbe0d6f03b8689a
   ],
   imports: [
     BrowserModule,
